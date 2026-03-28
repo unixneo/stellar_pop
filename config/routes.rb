ea48854724035cad414dc7bff11d4f08b9f9eb81
root <root@www.unix.com>
2026-03-28 20:33:23 +0700

    fix: favicon served reliably via svg icon tags and /favicon.ico redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  get "/favicon.ico", to: redirect("/favicon.svg")
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
